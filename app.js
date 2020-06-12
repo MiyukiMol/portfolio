d3df0c6346ca59506290718c650f05297c3d3fba
MiyukiMol <nantes211@gmail.com>
2020-06-12 17:16:48 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const heading = "Hello, I'm Miyuki";
+let i = 0;
+
+const typing = () => {
+    if(i < heading.length) {
+        document.querySelector('.heading').innerHTML += heading.charAt(i);
+        i++;
+
+        setTimeout(typing, 150);
+    }
+}
+
+typing();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
